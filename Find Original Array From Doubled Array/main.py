26d3f9bf4618c167dd604fd3df8ee198b56949ee
Zygmut <deltas.game@gmail.com>
2022-09-16 10:25:03 +0200

    Optimizations :)

--- Find Original Array From Doubled Array/main.py
+++ Find Original Array From Doubled Array/main.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/find-original-array-from-doubled-array/
-from bisect import bisect_left
 
 
 class Solution:
@@ -7,23 +6,34 @@ class Solution:
         if not (1 <= len(changed) <= 10**5):
             raise ValueError("Length of the array must be within [1, 10**5]")
 
-        sol = []
-        changed.sort()
+        if len(changed) % 2:
+            return []
 
-        def binary_search(arr: list[int], val: int) -> int | None:
-            i = bisect_left(arr, val)
-            if i != len(arr) and arr[i] == val:
-                return i
+        sol = []
+        changed.sort(reverse=True)
+
+        def rev_binary_search(arr: list[int], val: int) -> int | None:
+            lo, hi = 0, len(arr)
+            while lo < hi:
+                mid = (lo + hi) // 2
+                # change arr[mid] < val to ">" to port func to reverse ordered arrays
+                if arr[mid] > val:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            
+            if lo != len(arr) and arr[lo] == val:
+                return lo
             else:
                 return None
 
         while changed:
-            val = changed.pop(0)
+            val = changed.pop()
 
             if not (0 <= val <= 10**5):
                 raise ValueError("Values of the array must be within [0, 10**5]")
 
-            loc = binary_search(changed, val * 2)
+            loc = rev_binary_search(changed, val * 2)
 
             if loc is not None:
                 sol.append(val)
