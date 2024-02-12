414f1fdd0708b2596dee31af79eb6a78524fbdb3
Zygmut <deltas.game@gmail.com>
2024-02-13 00:49:21 +0100

    [ py ] Created base cases

---
+++ Rotate Array/main.py
@@ -0,0 +1,17 @@
+def rotate(nums: list[int], k: int):
+    return nums[-k:] + nums[: len(nums) - k]
+
+
+if __name__ == "__main__":
+    assert all(
+        map(
+            lambda values: rotate(values[0], values[1]) == values[2],
+            (
+                ([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]),
+                ([-1, -100, 3, 99], 2, [3, 99, -1, -100]),
+                ([1], 0, [1]),
+                ([1,2], 0, [1,2])
+            ),
+        )
+    )
+
