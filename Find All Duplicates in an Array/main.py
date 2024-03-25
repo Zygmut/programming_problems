7d088a6e701ae2aa2b92791e12e42761cadf3a02
Zygmut <deltas.game@gmail.com>
2024-03-25 20:39:39 +0100

    [ 🐍 ] Daily leet

--- Find All Duplicates in an Array/main.py
+++ Find All Duplicates in an Array/main.py
@@ -7,6 +7,20 @@ from collections import Counter
 
 
 def findDuplicates(nums: List[int]) -> List[int]:
+    sol = []
+
+    for num in nums:
+        index = abs(num)
+        if nums[index - 1] < 0:
+            sol.append(index)
+            continue
+
+        nums[index - 1] *= -1
+
+    return sol
+
+
+def findDuplicates2(nums: List[int]) -> List[int]:
     return [num for num, freq in Counter(nums).items() if freq > 1]
 
 
@@ -14,7 +28,15 @@ if __name__ == "__main__":
     testcases: List[Tuple[List[int], List[int]]] = [
         ([4, 3, 2, 7, 8, 2, 3, 1], [2, 3]),
         ([1, 1, 2], [1]),
-        ([3], []),
+        ([1], []),
     ]
 
-    assert all(findDuplicates(values) == expected for values, expected in testcases)
+    assert all(
+        all(
+            (
+                sorted(findDuplicates(values)) == sorted(expected),
+                sorted(findDuplicates2(values)) == sorted(expected),
+            )
+        )
+        for values, expected in testcases
+    )
