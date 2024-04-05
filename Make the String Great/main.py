24dce872c48079c352183a6f13bf9612564e5961
Zygmut <deltas.game@gmail.com>
2024-04-05 13:23:04 +0200

    [ 🐍 ] Daily leet

---
+++ Make the String Great/main.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.path.append("..")
+
+from typing import Tuple, List, Any
+
+
+def makeGood(s: str) -> str:
+    stack: List[str] = []
+
+    for character in s:
+        if stack and abs(ord(stack[-1]) - ord(character)) == 32:
+            stack.pop()
+            continue
+
+        stack.append(character)
+
+    return "".join(stack)
+
+
+if __name__ == "__main__":
+    testcases: List[Tuple[str, str]] = [
+        ("leEeetcode", "leetcode"),
+        ("abBAcC", ""),
+        ("pP", ""),
+    ]
+
+    assert all(makeGood(values) == expected for values, expected in testcases)
