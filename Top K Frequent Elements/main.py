88e933de6de59b47afbcf64622dce5c9c69d8680
Zygmut <40397192+Zygmut@users.noreply.github.com>
2024-04-15 16:04:15 +0200

    [ 🐍 ]

---
+++ Top K Frequent Elements/main.py
@@ -0,0 +1,3 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [pair[0] for pair in sorted(list(Counter(nums).items()), key = lambda x: -x[1])[:k]]
