460add71bef17b8138afef1f1d560bcba370b425
Zygmut <deltas.game@gmail.com>
2024-12-30 00:10:41 +0100

    direnv & add rust

--- shell.nix
+++ shell.nix
@@ -1,11 +1,15 @@
-{ pkgs? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
+{ pkgs? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11") {} }:
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     pre-commit
 
     python3
+
     go
+
+    cargo
+    rustc
   ];
 
   shellHook = ''
