388c417b940c7d2e98f23ab15f7460d75e6200d4
Zygmut <deltas.game@gmail.com>
2025-01-15 23:02:38 +0100

    Do not mutate argument :)

--- Minimize XOR/python/main.py
+++ Minimize XOR/python/main.py
@@ -1,23 +1,20 @@
 from typing import Tuple, List, Any
+from functools import reduce
 
 
 def fn(num1: int, num2: int) -> int:
-
     diff = num2.bit_count() - num1.bit_count()
 
+    if diff == 0:
+        return num1
+
     if diff > 0:
-
         # replace 0 to 1 from the least significat bit from nb1 as many times as nb2 - nb1
-        for _ in range(diff):
-            num1 |= (num1 + 1)
+        return reduce(lambda acc, _: acc | acc + 1, range(diff), num1)
 
-    else:
+    # Replace 1 to 0 from the least significant bit from nb1 as many times as nb1 - nb2
+    return reduce(lambda acc, _: acc & acc - 1, range(-diff), num1)
 
-        # Replace 1 to 0 from the least significant bit from nb1 as many times as nb1 - nb2
-        for _ in range(-diff):
-            num1 &= (num1 - 1)
-
-    return num1
 
 if __name__ == "__main__":
     testcases: List[Tuple[Any, Any]] = [
